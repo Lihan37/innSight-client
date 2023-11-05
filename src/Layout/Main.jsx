eeff5a88539ecc2fae9f4b776f2010be6780060e
Eanur Lihan <lihan.lihu@gmail.com>
2023-11-05 17:46:51 +0600

    I have added navbar and footer

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,17 @@
 import { Outlet } from "react-router-dom";
-
+import Footer from "../shared/Footer/Footer";
+import NavBar from "../shared/NavBar/NavBar";
 
 const Main = () => {
     return (
-        <div className="font-poppins">
+        <div className="font-poppins relative min-h-screen">
+            <NavBar></NavBar>
+            
             <Outlet></Outlet>
+            
+            <div className="absolute inset-x-0 bottom-0 bg-white">
+                <Footer></Footer>
+            </div>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,9 @@ import {
 import router from './Routes/Routes.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-     <RouterProvider router={router} />
-  </React.StrictMode>,
+  <div className='bg-gradient-to-r from-gray-950 to-purple-950'>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  </div>
 )
