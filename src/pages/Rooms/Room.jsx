01f02f9d4827497d3863602c3c6fc831bedc0a29
Eanur Lihan <lihan.lihu@gmail.com>
2023-11-07 00:51:41 +0600

    i added the sort high to low

--- src/pages/Rooms/Room.jsx
+++ src/pages/Rooms/Room.jsx
@@ -7,13 +7,13 @@ const Room = ({ room }) => {
   return (
     <div className="mb-5">
       <div className="card bg-blue-950 card-compact p-5 h-96 w-96 shadow-2xl shadow-orange-950">
-        <figure className="h-full">
-          <img src={image} alt="Shoes" />
+        <figure>
+          <img className="h-full rounded-sm w-full" src={image} alt="Room" />
         </figure>
         <div className="card-body h-1/2">
           <h2 className="card-title text-yellow-200 font-medium text-xl">{roomType}</h2>
-          <p className='text-blue-300 text-lg'>Price: {price}$</p>
-          <p className='text-blue-300 text-lg'>Total Reviews:</p>
+          <p className="text-blue-300 text-lg">Price: ${price}</p>
+          <p className="text-blue-300 text-lg">Total Reviews:</p>
           <div className="card-actions justify-end">
             <button className="btn bg-purple-900 text-yellow-200 hover:text-gray-600">
               Show Details <FaArrowRight />
--- src/pages/Rooms/Rooms.jsx
+++ src/pages/Rooms/Rooms.jsx
@@ -1,36 +1,59 @@
 import React, { useEffect, useState } from 'react';
-import NavBar from '../../shared/NavBar/NavBar';
 import Room from './Room';
 
 const Rooms = () => {
+  const [rooms, setRooms] = useState([]);
+  const [sortOrder, setSortOrder] = useState('default');
 
-    const [rooms, setRooms] = useState([]);
+  useEffect(() => {
+    fetch('rooms.json')
+      .then((res) => res.json())
+      .then((data) => setRooms(data));
+  }, []);
 
-    useEffect(() => {
-        fetch('rooms.json')
-            .then(res => res.json())
-            .then(data =>
-                setRooms(data))
-    }, [])
+  const handleSort = (order) => {
+    if (order === 'default') {
+      setSortOrder('default');
+      return;
+    }
 
-    return (
-        <div className='mt-10' >
-            <div className='px-20 w-3/4 mx-auto'>
-                <h1 className="text-4xl font-bold mb-4 text-center text-yellow-200">Discover Our Luxurious Rooms</h1>
-                <p className="text-lg text-center text-blue-300 mb-8">
-                    Experience comfort and elegance in our carefully designed rooms. Whether you're looking for a cozy single room or a spacious ocean-view suite, we have the perfect accommodations to make your stay memorable. Explore our room offerings below and start planning your dream getaway.
-                </p>
-            </div>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 pl-5 mb-10 mx-auto'>
-                {
-                    rooms.map(room => <Room
-                        key={room.id}
-                        room={room}
-                    ></Room>)
-                }
-            </div>
+    const sortedRooms = [...rooms];
+    if (order === 'highToLow') {
+      sortedRooms.sort((a, b) => b.price - a.price);
+    } else if (order === 'lowToHigh') {
+      sortedRooms.sort((a, b) => a.price - b.price);
+    }
+    setRooms(sortedRooms);
+    setSortOrder(order);
+  };
+
+  return (
+    <div className="mt-10">
+      <div className="px-20 w-3/4 mx-auto">
+        <h1 className="text-4xl font-bold mb-4 text-center text-yellow-200">
+          Discover Our Luxurious Rooms
+        </h1>
+        <p className="text-lg text-center text-blue-300 mb-8">
+          Experience comfort and elegance in our carefully designed rooms. Whether you're looking for a cozy single room or a spacious ocean-view suite, we have the perfect accommodations to make your stay memorable. Explore our room offerings below and start planning your dream getaway.
+        </p>
+        <div className="flex justify-center space-x-4 mb-4">
+          <select
+            value={sortOrder}
+            onChange={(e) => handleSort(e.target.value)}
+            className="bg-gray-300 text-gray-700 px-4 py-2 rounded"
+          >
+            <option value="highToLow">Price High to Low</option>
+            <option value="lowToHigh">Price Low to High</option>
+          </select>
         </div>
-    );
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 pl-5 mb-10 mx-auto">
+        {rooms.map((room) => (
+          <Room key={room.id} room={room} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Rooms;
